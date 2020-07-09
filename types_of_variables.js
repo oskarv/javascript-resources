d37e2f737575ec2b027abc81de4398c2c6aa45e0
Oskar <oskarsmit@hotmail.com>
2020-07-09 21:14:41 +0200

    Create types_of_variables.js

---
+++ types_of_variables.js
@@ -0,0 +1,23 @@
+let pi = 3.14; // primitive variable are immutable in JS !!!!
+
+// primitive immutatable types:
+// Number
+// String
+// BigInt
+// null
+// undefined
+// Symbol
+// boolean
+
+// complex mutable types:
+// objects
+// functions
+
+console.log(typeof pi);
+
+pi.math = 'maath';
+
+console.log(pi.math);
+
+// left side ... dot ... right side
+// left_side !== undefined && left_side !== null
